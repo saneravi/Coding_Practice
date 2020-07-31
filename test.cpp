9d72ef8a39aed4a016e7dcb406e7bf9f78214125
saneravi <ravisane73@gmail.com>
2020-07-31 11:10:41 +0530

    #include<b

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ int main()
 { 
    ios_base::sync_with_stdio(false);cin.tie(NULL); 
    
-   #ifndef ONLINE_JUDGE 
+   #ifndef ONLINE_JUDGE
    freopen("input.txt", "r", stdin); 
    freopen("error.txt", "w", stderr); 
    freopen("output.txt", "w", stdout); 
